57849ffb9334c152e500b749fe9e009fff319110
Angel <ata@loriot.io>
2017-12-15 11:11:01 +0100

    I - player get & post

---
+++ server/routes/players.routes.js
@@ -0,0 +1,24 @@
+var Player = require('Player');
+var SoccerPlayer = require('SoccerPlayer');
+
+module.exports = function (app) {
+  app.route('/api/players/:id/soccer-players')
+    .get(function (request, res) {
+      Player.findById(request.param.id, function (error, response) {
+        return response;
+      }).then(function (result) {
+        return res.send(result);
+      });
+    })
+    .post(function (request, res) {
+      var newSoccerPlayer = new SoccerPlayer();
+
+      newSoccerPlayer.name = request.body.name;
+      newSoccerPlayer.points = request.body.points;
+      newSoccerPlayer.position = request.body.position;
+
+      newSoccerPlayer.save().then(function (result) {
+        return res.send(result);
+      });
+    });
+};
