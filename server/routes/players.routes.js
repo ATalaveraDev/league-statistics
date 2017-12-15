fba92fa8f3f22dbcd726c2fdc029d32a831558a9
Angel <ata@loriot.io>
2017-12-15 12:22:35 +0100

    I - add team

--- server/routes/players.routes.js
+++ server/routes/players.routes.js
@@ -22,22 +22,16 @@ module.exports = function (app) {
       });
     });
 
-  app.route('/api/players/:id/soccer-players')
+  app.route('/api/players/:id/team')
     .get(function (request, res) {
-      Player.findById(request.param.id, function (error, response) {
+      Player.findById(request.params.id, function (error, response) {
         return response;
       }).then(function (result) {
         return res.send(result);
       });
     })
     .post(function (request, res) {
-      var newSoccerPlayer = new SoccerPlayer();
-
-      newSoccerPlayer.name = request.body.name;
-      newSoccerPlayer.points = request.body.points;
-      newSoccerPlayer.position = request.body.position;
-
-      newSoccerPlayer.save().then(function (result) {
+      Player.findByIdAndUpdate(request.params.id, { team: request.body.team }).then(function (result) {
         return res.send(result);
       });
     });
