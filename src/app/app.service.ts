02f9d3c63f9f7adfa919f572f0ecaa97967f495c
Angel <ata@loriot.io>
2017-12-22 12:39:06 +0100

    R - players variable using observable

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,12 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 
 @Injectable()
 export class AppService {
-  players: Array<any>;
+  private playersSubject = new ReplaySubject<any>();
+  players$ = this.playersSubject.asObservable();
 
   constructor(private http: HttpClient) {
     this.http.get('http://localhost:8080/api/players')
-      .subscribe((players: Array<any>) => this.players = players);
+      .subscribe((players: Array<any>) => this.playersSubject.next(players));
   }
 }
