34aecd473a3c55039e98344b10c3a77da29467e8
Angel <ata@loriot.io>
2018-02-14 18:06:47 +0100

    I - get players

--- server/routes/players.routes.js
+++ server/routes/players.routes.js
@@ -1,14 +1,16 @@
 var Player = require('../models/player');
+const https = require('https');
+var constants = require('../constants.js');
 
 module.exports = function (app) {
   app.route('/api/players')
-    .get(function (request, res) {
+    /*.get(function (request, res) {
       Player.find(function (error, result) {
         return result;
       }).then(function (result) {
         return res.send(result);
       });
-    })
+    })*/
     .post(function (request, res) {
       var newPlayer = new Player();
 
@@ -69,4 +71,40 @@ module.exports = function (app) {
         });
       });
     });
+
+
+
+
+  app.route('/api/players')
+    .get(function (request, response) {
+      getPlayers(request, function (result) {
+        response.send(result);
+      });
+    });
+
+  app.route('/api/players/:playerId')
+    .get(function (request, response) {
+      getPlayers(request, function (result) {
+        response.send(findPlayer(request.params.playerId, result));
+      });
+    });
+
+  function getPlayers(request, result) {
+    var data = '';
+    https.get({hostname: constants.hostname, path: constants.playersPath, headers: {'Authorization': 'Bearer ' + request.headers.bearer}}, function (response) {
+      response.on('data', function (chunk) {
+        data += chunk;
+      });
+
+      response.on('end', function () {
+        result(JSON.parse(data));
+      })
+    });
+  }
+
+  function findPlayer(playerId, players) {
+    return players.find(function (player) {
+      return player.id === playerId;
+    });
+  }
 };
